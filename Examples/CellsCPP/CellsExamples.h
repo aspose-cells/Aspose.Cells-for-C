842126a42cb2f63f40b50bad325040c628bbee4c
Shakeel Faiz <shakeel.faiz@aspose.com>
2017-05-14 10:34:59 +0500

    Uploading Sample Excel Files Used in Code

---
+++ Examples/CellsCPP/CellsExamples.h
@@ -0,0 +1,91 @@
+#include "Aspose.Cells.h"
+using namespace std;
+
+//Utility method to show message on console like printf
+void ShowMessageOnConsole(char* msg);
+
+//Developer Guide > Loading, Saving and Converting
+//Opening Files
+void OpeningExcelFileUsingPath();
+void OpeningExcelFileUsingStream();
+
+//Saving Files
+void SavingFiletoSomeLocation();
+void SavingFiletoStream();
+
+//Managing Document Properties
+void ManagingDocumentProperties();
+
+//Read and Write CSV File Format
+void ReadAndWriteCSVFileFormat();
+
+//Read and Write Tab Delimited File Format
+void ReadAndWriteTabDelimitedFileFormat();
+
+//Read and Write XLSB File Format 
+void ReadAndWriteXLSBFileFormat();
+
+//Read and Write XLSM File Format
+void ReadAndWriteXLSMFileFormat();
+
+//Developer Guide > Rows and Columns
+//Adjusting Row Height and Column Width
+//Setting the Height of a Row
+void SettingHeightOfRow();
+
+//Setting the Height of All Rows in a Worksheet
+void SettingHeightOfAllRowsInWorksheet();
+
+//Setting the Width of a Column
+void SettingWidthOfColumn();
+
+//Setting the Width of All Columns in a Worksheet
+void SettingWidthOfAllColumnsInWorksheet();
+
+//Copying Rows and Columns
+//Copying Rows
+void CopyingRows();
+
+//Copying Columns
+void CopyingColumns();
+
+//Grouping, Ungrouping Rows and Columns
+//Grouping Rows & Columns
+void GroupingRowsColumns();
+
+//Ungrouping Rows & Columns
+void UnGroupingRowsColumns();
+
+//Inserting, Deleting Rows and Columns
+//Insert a Row
+void InsertRow();
+
+//Inserting Multiple Rows
+void InsertingMultipleRows();
+
+//Deleting Multiple Rows
+void DeletingMultipleRows();
+
+//Insert a Column
+void InsertColumn();
+
+//Delete a Column
+void DeleteColumn();
+
+//Developer Guide > Data
+//Accessing Cells
+//Using Cell Name
+void AccessingCellsUsingCellName();
+
+//Using Row & Column Index of the Cell
+void AccessingCellsUsingRowAndColumnIndexOfTheCell();
+
+//Maximum Display Range of Worksheet
+void AccessingMaximumDisplayRangeOfWorksheet();
+
+//Add and Retrieve Data
+//Adding Data to Cells
+void AddingDataToCells();
+
+//Retrieving Data from Cells
+void RetrievingDataFromCells();
---
+++ Examples/CellsCPP/main.cpp
@@ -0,0 +1,127 @@
+﻿/*The unique head file need to be included*/
+#include "Aspose.Cells.h"
+#include "CellsExamples.h"
+
+using namespace std;
+
+void ShowMessageOnConsole(char* msg)
+{
+	StringPtr msgConsole = new String(msg);
+	Console::WriteLine(msgConsole);
+}
+
+void ShowCellsVersion()
+{
+	printf("Aspose.Cells for C++ Version: %s\n\n", ICellsHelper::GetVersion()->charValue());
+}
+
+int main(int argc, char** argv)
+{
+	//Show Aspose.Cells for C++ Version
+	ShowCellsVersion();
+	
+	//Developer Guide > Loading, Saving and Converting
+	//--------------------------------------------------------------------------------------------------------------------------
+	Console::WriteLine(new String("Section: Developer Guide > Loading, Saving and Converting"));
+	Console::WriteLine(new String("--------------------------------------------------------------"));
+	Console::WriteLine(new String("--------------------------------------------------------------\r\n"));
+
+	//Opening Files
+	OpeningExcelFileUsingPath();
+	OpeningExcelFileUsingStream();
+
+	//Saving Files
+	SavingFiletoSomeLocation();
+	SavingFiletoStream();
+
+	//Managing Document Properties
+	ManagingDocumentProperties();
+
+	//Read and Write CSV File Format
+	ReadAndWriteCSVFileFormat();
+
+	//Read and Write Tab Delimited File Format
+	ReadAndWriteTabDelimitedFileFormat();
+
+	//Read and Write XLSB File Format 
+	ReadAndWriteXLSBFileFormat();
+
+	//Read and Write XLSM File Format
+	ReadAndWriteXLSMFileFormat();
+
+	//Developer Guide > Rows and Columns
+	//--------------------------------------------------------------------------------------------------------------------------
+	Console::WriteLine(new String("Section: Developer Guide > Rows and Columns"));
+	Console::WriteLine(new String("--------------------------------------------------------------"));
+	Console::WriteLine(new String("--------------------------------------------------------------\r\n"));
+	
+	//Adjusting Row Height and Column Width
+	//Setting the Height of a Row
+	SettingHeightOfRow();
+
+	//Setting the Height of All Rows in a Worksheet
+	SettingHeightOfAllRowsInWorksheet();
+
+	//Setting the Width of a Column
+	SettingWidthOfColumn();
+
+	//Setting the Width of All Columns in a Worksheet
+	SettingWidthOfAllColumnsInWorksheet();
+
+	//Copying Rows and Columns
+	//Copying Rows
+	CopyingRows();
+
+	//Copying Columns
+	CopyingColumns();
+
+	//Grouping, Ungrouping Rows and Columns
+	//Grouping Rows & Columns
+	GroupingRowsColumns();
+
+	//Ungrouping Rows & Columns
+	UnGroupingRowsColumns();
+
+	//Inserting, Deleting Rows and Columns
+	//Insert a Row
+	InsertRow();
+
+	//Inserting Multiple Rows
+	InsertingMultipleRows();
+
+	//Deleting Multiple Rows
+	DeletingMultipleRows();
+
+	//Insert a Column
+	InsertColumn();
+
+	//Delete a Column
+	DeleteColumn();
+
+	//Developer Guide > Data
+	//--------------------------------------------------------------------------------------------------------------------------
+	Console::WriteLine(new String("Section: Developer Guide > Data"));
+	Console::WriteLine(new String("--------------------------------------------------------------"));
+	Console::WriteLine(new String("--------------------------------------------------------------\r\n"));
+
+	//Accessing Cells
+	//Using Cell Name
+	AccessingCellsUsingCellName();
+
+	//Using Row & Column Index of the Cell
+	AccessingCellsUsingRowAndColumnIndexOfTheCell();
+
+	//Maximum Display Range of Worksheet
+	AccessingMaximumDisplayRangeOfWorksheet();
+
+	//Add and Retrieve Data
+	//Adding Data to Cells
+	AddingDataToCells();
+
+	//Retrieving Data from Cells
+	RetrievingDataFromCells();
+
+	// Stop before exiting
+	printf("\n\nProgram Finished. Press any key to exit....");
+	getchar();
+}
