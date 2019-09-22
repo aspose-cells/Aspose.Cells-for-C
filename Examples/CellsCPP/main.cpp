567e22eccfa4816fc3fe666077ca6e8888d3c955
Ahmad <m.ahmadchishti@gmail.com>
2019-09-22 19:50:46 +0500

    Update main.cpp

--- Examples/CellsCPP/main.cpp
+++ Examples/CellsCPP/main.cpp
@@ -22,22 +22,94 @@ int main(int argc, char** argv)
 
 	//Uncomment the one you want to try out
 
-	//Aspose.Cells for C++ v18.5
-	ConvertExcelWorkbookToPDF_A_CompliedFiles();
-	ConvertExcelWorkbookToPDF_DirectConversion();
-	ConvertExcelWorkbookToPDF_SetPDFCreationTime();
-	ConvertingWorksheetToImage_PNG();
-	ConvertingWorksheetToImage_SVG();
-	ConvertingWorksheetToImage_TIFF();
-	
-	//Aspose.Cells for C++ v18.4
-	//ReadAndManipulateExcel2016Charts();
+	// Charts
 	//CreatingAndCustomizingCharts_BubbleChart();
 	//CreatingAndCustomizingCharts_CustomChart();
 	//CreatingAndCustomizingCharts_LineChart();
 	//CreatingAndCustomizingCharts_PyramidChart();
+	//ReadAndManipulateExcel2016Charts();
+
+	// Data
+	//AccessingCellsUsingCellName();
+	//AccessingCellsUsingRowAndColumnIndexOfTheCell();
+	//AccessingMaximumDisplayRangeOfWorksheet();
+	//AddingDataToCells();
+	//RetrievingDataFromCells();
+	//AddHyperlinksToTheCells();
+	//ApplyConditionalFormattingInWorksheet();
+	//FormatCellOrRangeOfCells();
+	//CreateNamedRangeInWorkbook();
+	//CreatingSubtotals();
+	//FindOrSearchData();
+	//ManipulateNamedRangeInWorkbook();
+	//TracingPrecedents();
+	//TracingDependents();
+
+	// DrawingObjects
 	//ExtractingOLEObjectsFromWorksheet();
 	//InsertingOLEObjectsIntoWorksheet();
+
+	// Formulas
+	//CalculateWorkbookFormulas();
+	//UsingICustomFunctionFeature();
+	//AddingFormulasAndCalculatingResults();
+	//DirectCalculationOfFormula();
+	//CalculatingFormulasOnceOnly();
+
+	// LoadingSavingAndConverting
+	//ConvertExcelWorkbookToPDF_A_CompliedFiles();
+	//ConvertExcelWorkbookToPDF_DirectConversion();
+	//ConvertExcelWorkbookToPDF_SetPDFCreationTime();
+	//ConvertingWorksheetToImage_PNG();
+	//ConvertingWorksheetToImage_SVG();
+	//ConvertingWorksheetToImage_TIFF();
+	//ManagingDocumentProperties();
+	//OpeningExcelFileUsingPath();
+	//OpeningExcelFileUsingStream();
+	//ReadAndWriteCSVFileFormat();
+	//ReadAndWriteTabDelimitedFileFormat();
+	//ReadAndWriteXLSBFileFormat();
+	//ReadAndWriteXLSMFileFormat();
+	//SavingFiletoSomeLocation();
+	//SavingFiletoStream();
+
+	// PivotTables
+	//CreatePivotTable();
+	//ManipulatePivotTable();
+
+	// RowsAndColumns
+	//SettingHeightOfRow();
+	//SettingHeightOfAllRowsInWorksheet();
+	//SettingWidthOfColumn();
+	//SettingWidthOfAllColumnsInWorksheet();
+	//CopyingRows();
+	//CopyingColumns();
+	//GroupingRowsColumns();
+	//UnGroupingRowsColumns();
+	//InsertRow();
+	//InsertingMultipleRows();
+	//DeletingMultipleRows();
+	//InsertColumn();
+	//DeleteColumn();
+
+	// TechnicalArticles
+	//ApplyCustomThemeColorsOfWorkbookUsingArrayOfColors();
+	//CopyThemeFromOneWorkbookToAnother();
+	//CreateAndManipulateExcelTable();
+	//GroupRowsAndColumnsOfWorksheet();
+
+	// Worksheets
+	//CopyWorksheetsWithinWorkbook();
+	//MoveWorksheetsWithinWorkbook();
+	//AddingWorksheetsToNewExcelFile();
+	//AccessingWorksheetsUsingSheetIndex();
+	//RemovingWorksheetsUsingSheetIndex();
+	//AddingPageBreaks();
+	//EnablingPageBreakPreview();
+	//ZoomFactor();
+	//FreezePanes();
+	//SplitPanes();
+	//RemovingPanes();
 
 	//Stop before exiting
 	printf("\nProgram Finished. Press Enter to Exit....");
